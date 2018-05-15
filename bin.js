5ec8caa835373db2fcda77e284248bc48dec388d
Vincent Ahrend <mail@vincentahrend.com>
2018-05-15 18:49:51 +0200

    Import config from file

--- bin.js
+++ bin.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
+var fs = require('fs');
 const {createClient} = require('./index')
+
+const config = JSON.parse(fs.readFileSync('/Users/pv/.dat/pinning.config', 'utf8'));
+debugger;
 
 go(...(process.argv.slice(2)))
 
-function go (serviceUrl, username, password, action, ...args) {
-  console.log('Connecting to', serviceUrl)
-  createClient(serviceUrl, {username, password}, (err, client) => {
+function go (action, ...args) {
+  console.log('Connecting to', config["serverUrl"])
+  createClient(config["serverUrl"], {username: config["username"], password: config["password"]}, (err, client) => {
     if (err) return onerror(err)
 
     console.log(action, '(', args.map(JSON.stringify), ')')
     switch (action) {
-      case 'addDat': return client.addDat({url: args[0], name: args[1], domains: args.slice(2)}, cb)
-      case 'updateDat': return client.updateDat(args[0], {name: args[1], domains: args.slice(2)}, cb)
-      case 'removeDat': return client.removeDat(args[0], cb)
-      case 'getDat': return client.getDat(args[0], cb)
+      case 'add': return client.addDat({url: args[0], name: args[1], domains: args.slice(2)}, cb)
+      case 'update': return client.updateDat(args[0], {name: args[1], domains: args.slice(2)}, cb)
+      case 'remove': return client.removeDat(args[0], cb)
+      case 'get': return client.getDat(args[0], cb)
+      case 'list': return client.listDats(cb)
       default: return client[action](cb)
     }
     function cb (err, res) {
